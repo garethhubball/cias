cce081606dd107d9cda0e941e58958dcbd21d4bc
Cazgem <cazgem@gmail.com>
2020-09-12 00:24:19 +0000

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # cias
+
+```javascript
+npm install cias
+
+//Required Dependencies
+
+npm install mysql
+npm install tmi.js
+npm install polyphony.js
+npm install obs-websocket-js
+```
+
+This library is meant to act as an extension to polyphony.js to allow for the smooth management and performance of the weekly city-building competition, Cities in a Snap and other E-Sport events.
+
+## BEFORE YOU BEGIN
+Make a MySQL database table with at least two columns, number and name to correspond to the four participant spots. This will be more flexible in the future, but for now this is how it is.
+
+
+Then run the `twitchCPR.list` function (details below) to get your individual reward IDs.
+
+## Implementation
+
+### Includes
+```javascript
+
+const tmi = require('tmi.js'); // Recommended for chat functionality, though not strictly necessary to function.
+const mysql = require(`mysql`);
+const config = require('./config'); // Great to store variables safely
+```
+
+### Building the Config
+```javascript
+    const ciasOPTS = {
+        OBSaddress: config.default.obs_address, //Your Host Computer's IP address and websocket port no. (4444 by default)
+        OBSpassword: config.default.obs_pass, // Your OBS websocket password (If it exists!)
+        MYSQLhost: config.mysql.host, // The location of your database (either localhost, or an IP address if a different server location)
+        MYSQLuser: config.mysql.user, // MySQL Username
+        MYSQLpassword: config.mysql.password, // MySQL Password
+        MYSQLdatabase: config.mysql.database, // MySQL Database Name
+        MYSQLtable: `CiaS_Participants` // Participants table name within your database
+    }
+```
+
+## USE
+
+### Announcements
+Announces the input text to all participants in an event.
+
+```javascript
+cias.announce(client, msg, context, channel, ciasOPTS);
+```
+
+### Participants
+Calls and Alters the Participant Database.
+
+```javascript
+cias.participants(client, params, context, channel, ciasOPTS);
+```
+
+### Ten Seconds Remaining
+Calls and Alters the Participant Database.
+
+```javascript
+cias.tenseconds(client, ciasOPTS);
+```
+
+### Starting Timer (30 seconds)
+Calls and Alters the Participant Database.
+
+```javascript
+cias.starting(client, ciasOPTS);
+```
+
+
+Developed by Cazgem (https://twitch.tv/cazgem) for use as part of cities in a snap (https://twitch.tv/citiesinasnap) as well as for his chatbot, Polyphony.
---
+++ index.js
@@ -0,0 +1,299 @@
+//cias helper
+const OBSWebSocket = require('obs-websocket-js');
+const obs = new OBSWebSocket();
+const mysql = require(`mysql`);
+const polyphony = require('polyphony.js');
+module.exports = {
+    announce: function (client, msg, context, channel, ciasOPTS) {
+        if (context.mod || (context["user-id"] === context["room-id"])) {
+            const db = mysql.createConnection({
+                host: ciasOPTS.MYSQLhost,
+                user: ciasOPTS.MYSQLuser,
+                password: ciasOPTS.MYSQLpassword,
+                database: ciasOPTS.MYSQLdatabase
+            });
+            let sql = `SELECT name FROM ${ciasOPTS.MYSQLtable}`;
+            let response = db.query(sql, (err, result) => {
+                if (err) throw err;
+                console.log(err);
+                console.log(msg.slice(9));
+                Object.keys(result).forEach(function (id) {
+                    client.action(result[id].name, msg.slice(9));
+                    console.log(msg.slice(9));
+                });
+            });
+            db.end();
+        }
+    },
+    OBS_RefreshParticipants: async function (participant, ciasOPTS) {
+        const obs = new OBSWebSocket();
+        let source = `Participant ${participant} Screen`;
+        let source2 = `Participant Name ${participant}`;
+        let source3 = `NamePlate ${participant}`;
+        obs.connect({
+            address: ciasOPTS.OBSaddress,
+            password: ciasOPTS.OBSpassword
+        })
+            .then(data => {
+                obs.send('SetSceneItemProperties', {
+                    item: source,
+                    visible: false
+                })
+                setTimeout(() => {
+                    obs.send('SetSceneItemProperties', {
+                        item: source,
+                        visible: true
+                    })
+                }, 2000);
+            })
+            .then(data => {
+                obs.send('SetSceneItemProperties', {
+                    item: source2,
+                    visible: false
+                })
+                setTimeout(() => {
+                    obs.send('SetSceneItemProperties', {
+                        item: source2,
+                        visible: true
+                    })
+                }, 2000);
+            })
+            .then(data => {
+                obs.send('SetSceneItemProperties', {
+                    item: source3,
+                    visible: false
+                })
+                setTimeout(() => {
+                    obs.send('SetSceneItemProperties', {
+                        item: source3,
+                        visible: true
+                    })
+                }, 2000);
+            })
+            .catch(err => { // Promise convention dicates you have a catch on every chain.
+                console.log(err);
+            });
+    },
+    addParticipant: function (number, name, channel, client, ciasOPTS) {
+        const db = mysql.createConnection({
+            host: ciasOPTS.MYSQLhost,
+            user: ciasOPTS.MYSQLuser,
+            password: ciasOPTS.MYSQLpassword,
+            database: ciasOPTS.MYSQLdatabase
+        });
+        let sql = `UPDATE ${ciasOPTS.MYSQLtable} SET name = "${name}" WHERE number = "${number}"`;
+        let query = db.query(sql, (err, result) => {
+            if (err) throw err;
+        });
+        console.log('Participant Added');
+        console.log('Participant URL Identified');
+        this.updateURL(number, name, ciasOPTS);
+        console.log("Preparing to Refresh nameplates");
+        this.OBS_RefreshParticipants(number, ciasOPTS);
+        client.action(channel, 'Edited Participant ' + number + ' information.');
+        client.action(name, `Hey there, ${name}! I've been sent by CiaS staff to help things run smoothly for you during this event. In order to ensure I can do my job, I need to be granted moderator privileges for the duration of the event. Thanks, and good luck!`);
+        console.log(channel, 'Edited Participant ' + number + ' information.');
+        db.end();
+    },
+    refreshParticipantNames: function (participant, ciasOPTS) {
+        let source = `Participant Name ${participant}`;
+        obs.connect({
+            address: ciasOPTS.OBSaddress,
+            password: ciasOPTS.OBSpassword
+        })
+            .then(() => {
+                console.log(`OBS Connection Established`);
+            })
+            .then(() => {
+                obs.send('SetSceneItemProperties', {
+                    item: source,
+                    visible: false
+                })
+                setTimeout(() => {
+                    obs.send('SetSceneItemProperties', {
+                        item: source,
+                        visible: true
+                    })
+                }, 1000);
+            })
+            .catch(err => { // Promise convention dicates you have a catch on every chain.
+                console.log(err);
+            });
+    },
+    updateURL: function (number, name, ciasOPTS) {
+        console.log(`OBS Prepping for updateURL`);
+        const obs = new OBSWebSocket();
+        obs.connect({
+            address: `${ciasOPTS.OBSaddress}`,
+            password: `${ciasOPTS.OBSpassword}`
+        })
+            .then(() => {
+                console.log(`OBS Connection Established for updateURL`);
+            })
+            .then(() => {
+                return obs.send('SetBrowserSourceProperties', {
+                    source: `Participant ${number} Screen`,
+                    url: `https://player.twitch.tv/?channel=${name}&parent=streamernews.example.com&muted=true`
+                })
+            })
+            .catch(err => { // Promise convention dicates you have a catch on every chain.
+                console.log(err);
+            });
+    },
+    participants: function (client, params, context, channel, ciasOPTS) {
+        const db = mysql.createConnection({
+            host: ciasOPTS.MYSQLhost,
+            user: ciasOPTS.MYSQLuser,
+            password: ciasOPTS.MYSQLpassword,
+            database: ciasOPTS.MYSQLdatabase
+        });
+        if ((params[0] === 'edit') && ((context['user-type'] === ('mod')) || (context["display-name"] === "citiesinasnap"))) {
+            this.addParticipant(params[1], params[2], channel, client, ciasOPTS);
+        } else if ((params[0] === 'new') && ((context['user-type'] === ('mod')) || (context["display-name"] === "citiesinasnap"))) {
+            this.addParticipant(1, params[1], channel, client, ciasOPTS)
+            setTimeout(() => {
+                this.addParticipant(2, params[2], channel, client, ciasOPTS);
+            }, 1000);
+            setTimeout(() => {
+                this.addParticipant(3, params[3], channel, client, ciasOPTS);
+            }, 2000);
+            setTimeout(() => {
+                this.addParticipant(4, params[4], channel, client, ciasOPTS);
+            }, 3000);
+        } else if ((params[0] === 'join') && ((context['user-type'] === ('mod')) || (context["display-name"] === "citiesinasnap"))) {
+            client.action(channel, "This round's Participants are: ");
+            let sql = `SELECT * FROM ${ciasOPTS.MYSQLtable}`;
+            let response = db.query(sql, (err, result) => {
+                if (err) throw err;
+                Object.keys(result).forEach(function (id) {
+                    client.join(result[id].name);
+                });
+            });
+        } else if ((params[0] === 'part') && ((context['user-type'] === ('mod')) || (context["display-name"] === "citiesinasnap"))) {
+            client.action(channel, "This round's Participants are: ");
+            let sql = `SELECT * FROM ${ciasOPTS.MYSQLtable}`;
+            let response = db.query(sql, (err, result) => {
+                if (err) throw err;
+                Object.keys(result).forEach(function (id) {
+                    client.part(result[id].name);
+                });
+            });
+        } else {
+            console.log(ciasOPTS.MYSQLtable);
+            client.action(channel, "This round's Participants are: ");
+            let sql = `SELECT * FROM ${ciasOPTS.MYSQLtable}`;
+            let response = db.query(sql, (err, result) => {
+                if (err) throw err;
+                Object.keys(result).forEach(function (id) {
+                    client.action(channel, "Participant no. " + result[id].number + ": https://twitch.tv/" + result[id].name);
+                });
+            });
+        }
+        db.end();
+    },
+    tenseconds: function (client, ciasOPTS) {
+        const db = mysql.createConnection({
+            host: ciasOPTS.MYSQLhost,
+            user: ciasOPTS.MYSQLuser,
+            password: ciasOPTS.MYSQLpassword,
+            database: ciasOPTS.MYSQLdatabase
+        });
+        let sql = `SELECT name FROM ${ciasOPTS.MYSQLtable}`;
+        let response = db.query(sql, (err, result) => {
+            if (err) throw err;
+            Object.keys(result).forEach(function (id) {
+                client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 10 seconds remain! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 ");
+                client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 10 seconds remain! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 ");
+                setTimeout(() => {
+                    client.action(result[id].name, "9 seconds");
+                }, 1000);
+                setTimeout(() => {
+                    client.action(result[id].name, "8");
+                }, 2000);
+                setTimeout(() => {
+                    client.action(result[id].name, "7");
+                }, 3000);
+                setTimeout(() => {
+                    client.action(result[id].name, "6");
+                }, 4000);
+                setTimeout(() => {
+                    client.action(result[id].name, "5");
+                }, 5000);
+                setTimeout(() => {
+                    client.action(result[id].name, "4");
+                }, 6000);
+                setTimeout(() => {
+                    client.action(result[id].name, "3");
+                }, 7000);
+                setTimeout(() => {
+                    client.action(result[id].name, "2");
+                }, 8000);
+                setTimeout(() => {
+                    client.action(result[id].name, "1");
+                }, 9000);
+                setTimeout(() => {
+                    client.action(result[id].name, "cities1Stop cities1Stop cities1Stop All building Must stop! cities1Stop cities1Stop cities1Stop ");
+                }, 10000);
+            });
+        });
+        db.end();
+    },
+    starting: function (client, db, ciasOPTS) {
+        let sql = `SELECT name FROM ${ciasOPTS.database}`;
+        let response = db.query(sql, (err, result) => {
+            if (err) throw err;
+            Object.keys(result).forEach(function (id) {
+                try {
+                    client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 30 seconds Until Start! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1");
+
+                } catch (err) {
+
+                }
+                setTimeout(() => {
+                    try {
+                        client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 20 seconds Until Start! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1");
+
+                    } catch (err) {
+
+                    }
+                }, 10000);
+                setTimeout(() => {
+                    try {
+                        client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 10 seconds Until Start! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1");
+
+                    } catch (err) {
+
+                    }
+                }, 20000);
+                setTimeout(() => {
+                    try {
+                        client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 5 seconds Until Start! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1");
+
+                    } catch (err) {
+
+                    }
+                }, 25000);
+                setTimeout(() => {
+                    try {
+                        client.action(result[id].name, "cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1 Begin! cities1Stopwatch1 cities1Stopwatch1 cities1Stopwatch1");
+
+                    } catch (err) {
+
+                    }
+                }, 30000);
+            });
+        });
+        db.end();
+    },
+    refreshParticipants: function (params, ciasOPTS) {
+        if (params[0] === `all`) {
+            this.OBS_RefreshParticipants(1, ciasOPTS);
+            this.OBS_RefreshParticipants(2, ciasOPTS);
+            this.OBS_RefreshParticipants(3, ciasOPTS);
+            this.OBS_RefreshParticipants(4, ciasOPTS);
+        } else {
+            this.OBS_RefreshParticipants(params[0], ciasOPTS);
+        }
+
+    }
+}
